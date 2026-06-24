a83560d82fb4f03c2c2fa864316869560afcfda0
skotturty-dotcom <skotturty@gmail.com>
2026-06-24 20:58:22 +0530

    fix: Update Menu anchor link to point to product showcase

--- src/data/navigation.js
+++ src/data/navigation.js
@@ -4,7 +4,7 @@ export const navigationLinks = [
   { label: "Products", path: "/products", id: "nav-products" },
   { 
     label: "Menu", 
-    path: "/#menus", 
+    path: "/#product-showcase", 
     id: "nav-menus",
     submenus: [
       { label: "Tea", path: "/products?category=Tea", id: "sub-tea" },
